5f7010e400b5b360607544305ed954885b106d37
am <andrea.marin2411@gmail.com>
2020-11-30 17:16:33 -0600

    change restrictions to g(x) > 0

--- rest.m
+++ rest.m
@@ -4,6 +4,8 @@ n = length(W)/2 + 1;
 k = 1;
 g = zeros(5*(n-1)+ n*(n-1)/2,1);
 
+% restricción para tener el diámetro es menor a 1
+% r_i^2 + r_j^2 - 2*r_i*r_j*cos(theta_j - theta_i) <= 1
 for i = 1:(n-1)
     ri = W(i);
     g(k) = ri^2;
@@ -14,49 +16,53 @@ for i = 1:(n-1)
     ri = W(i);
     thetai = W(n-1+i);
     
-    for j = i:(n-1) 
+    for j = i+1:(n-1) 
      rj = W(j);
      thetaj = W(n-1+j);
     
-     g(k) = ri^2 + rj^2 - 2*ri*rj*cos(thetai-thetaj) - 1;
+     g(k) = 1 - (ri^2 + rj^2 - 2*ri*rj*cos(thetai-thetaj));
      k = k+1;
     end
 end
 
 
+% ángulos deben incrementarse conforme crece la i
 for i = 1:(n-2)
     thetai = W(n-1+i);
     thetai1 = W(n+i);
     
-    g(k) = thetai - thetai1;
+    g(k) = thetai1 - thetai;
     k = k+1;
 end
 
+% ángulo menor a pi
+for i = 1:(n-1)
+    thetai = W(n-1+i);
+    g(k) = pi - thetai;
+    k = k+1;
+end
+
+% ángulo mayor a cero
 for i = 1:(n-1)
     thetai = W(n-1+i);
     
-    g(k) = thetai - pi;
+    g(k) = thetai;
     k = k+1;
 end
 
-for i = 1:(n-1)
-    thetai = W(n-1+i);
-    
-    g(k) = - thetai;
-    k = k+1;
-end
-
+% radio menor a 1
 for i = 1:(n-1)
     ri = W(i);
     
-    g(k) = ri -1;
+    g(k) = 1-ri;
     k = k+1;
 end
 
+% radio mayor a cero
 for i = 1:(n-1)
     ri = W(i);
     
-    g(k) = -ri;
+    g(k) = ri;
     k = k+1;
 end
 
